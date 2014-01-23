06c721f8725f56384121339edefdd569b2822ea2
Pier Paolo Ramon <ramonpierre@gmail.com>
2014-01-23 18:14:46 +0100

    Copyright all the things

--- Classes/ItSmcSoftkeyboardModuleAssets.m
+++ Classes/ItSmcSoftkeyboardModuleAssets.m
@@ -1,6 +1,20 @@
 /**
- * This is a generated file. Do not edit or your changes will be lost
+ * Copyright (c) 2014 SMC Treviso s.r.l. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * Appcelerator Titanium is Copyright (c) 2009-2010 by Appcelerator, Inc.
+ * and licensed under the Apache Public License (version 2)
  */
+
 #import "ItSmcSoftkeyboardModuleAssets.h"
 
 extern NSData* filterDataInRange(NSData* thedata, NSRange range);
